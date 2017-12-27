01824e458640551b366509fc386521367b2fae52
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-12-27 15:57:08 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node 'ip-172-31-37-151.us-east-2.compute.internal' {
   include role::master_server
   file { '/root/README':
     ensure  => file,
-    content => $fqdn
+    content => "Welcome to ${fqdn}",
   }
 }
 node /^web/ {
