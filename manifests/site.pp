8cbd99b55ce8c28e1f12903b3839b25c365768eb
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-12-27 14:48:46 +0100

    Added web and db nodes

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,9 @@ node default {
 node 'ip-172-31-37-151.us-east-2.compute.internal' {
   include role::master_server
 }
+node /^web/ {
+  include role::app_server
+}
+node /^db/ {
+  include role::app_server
+}
