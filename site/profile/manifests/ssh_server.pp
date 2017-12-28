3fb29fd39ce3e1cd56fdcd01ba5a909be1cfd95e
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-12-28 15:21:39 +0100

    Updated SSH key

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCjaAUuwRVO4P4CsGzOaF17iykyX8i2ZzK921wgt5mvXC3cm7dzfnjz3XSY6fo8AUH7NJfb5YPwtjAi7s6boucNTLMKzwR0fP4fOnu8CEnONM7qepyFF57MNuZfXlNB9EiWyuUuImW1mNRqzfI+XgRJIFfHtwJX48tkk0OrIuFpu3PIC7FY4CLcKnKIAYj+2PbScdqA5xY3I9K8s8iuhlDG05IQ04rT38Bl8TiA9qkCiaAN8wHtYIzpqm7q5WQ/9Cf0ms3mE5fsSaMhSFn86rt3ISXbP8feKZDcXYa3XECsqw43INn521jk02hiByAUPyghCbEFcfuqscoTEgXpXfPz',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC2ijd3HpchPudFlHBmYrMe8ue7eRBg++t20s7vuro0uuFKgayuyNoumFoSRyNfsEr5PIxCSLvFRu9uj28uhmaKMHJu9qGdOngDLkJsZgOmjUARFi7zmQJXWpRFCtH3Ntz4OWYqqQj98X21MMC42DVvfxJsXdtRE6EpW1Lxc/YM/JNLfq/I2OsYu1pnj4OmDRpTMit9CvonrnLIUKMvio/ZoAexsQ4iMh73R6UWN5M4xN6PBl8GPxXGgpGn1b+mpfK9DrpG+nYXaQ58hZS/CvpVLP7Rpe/59MJwWSDL74fVF+oSqpYrQijG1rgl1A43HQrjkvGEerD+TobB1nT7wGGP',
   }
 }
