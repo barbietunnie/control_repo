39b2729265750f680283cabcbb671591959f84cf
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-12-28 16:20:51 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,9 +7,12 @@ node 'ip-172-31-37-151.us-east-2.compute.internal' {
     content => "Welcome to ${fqdn}\n",
   }
 }
+node 'minecraft.puppet.vm' {
+  include role::minecraft_server
+}
 node /^web/ {
   include role::app_server
 }
 node /^db/ {
-  include role::app_server
+  include role::db_server
 }
